5cbc33362b502386386b43c85ef6afffc2b0691f
igor <ingvarr.gomeniuk@gmail.com>
2022-11-22 21:56:47 +0100

    commit

---
+++ LESSONS/Lesson_7/script.js
@@ -0,0 +1,63 @@
+// alert('Hello World!');
+// console.log('Hello World!');
+// let first_name = "Igor"; //string
+// let num = 25; //number
+// let question = true; //boolean
+// const pi = 3.14;
+// console.log(first_name);
+// let result = prompt("Enter the number");
+// console.log(result);
+// let num = 25;
+// let str1 = "Hello";
+// let str2 = 'Hello';
+// let str3 = `Hello ${num} world`;
+// console.log(str3);
+// console.log(2 + 3);
+// console.log(4 - 2);
+// console.log(2 * 4);
+// console.log(6 / 2);
+// console.log(5 ** 2);
+// console.log(11 % 2);
+// let first_name = "Igor";
+// let last_name = "Gomeniuk";
+// console.log(first_name+" "+last_name);
+// console.log('My "NAME"');
+// let first_name = "Igor";
+// let greeting = "Hello!"
+// console.log(`${greeting}, ${first_name}`);
+// let num = 123;
+// let str = String(num);
+// console.log(typeof(num));
+// console.log(typeof(str));
+// let str = "123";
+// let num = +str;
+// console.log(typeof(num));
+// console.log(typeof(str));
+// let result = prompt("Enter the number");
+// console.log(result);
+// console.log('result' * 60);
+// let result = prompt("1985 * 60");
+// console.log('1985' * 60);
+// let a = (prompt("Enter the number"));
+// let b = (prompt("Enter the number"));
+// console.log(a|b);
+// let num = Number(prompt("Enter the number"));
+// if(num == 0){
+//     console.log("Nul")
+// }
+// else if(num > 0){
+//     console.log("положительное");
+// }
+// else{
+//     console.log("отрицательное");
+// }
+let num = Number(prompt("Enter the number"));
+if(num == 0){
+   console.log("Равно нулю")
+}
+else if(num == 1){
+    console.log("четное");
+}
+else{
+    console.log("не четное");
+}
