05216260548cec07524943942ef7fdb1494605e3
igor <ingvarr.gomeniuk@gmail.com>
2022-11-24 21:54:13 +0100

    commit

---
+++ LESSONS/Lesson_8/script.js
@@ -0,0 +1,68 @@
+// Array - Массивы
+
+// let fruits = ['Apple','Banana'];
+// console.log(fruits.length);
+
+// let first_fruit = fruits[fruits.length - 1];
+// console.log(first_fruit[0]);
+
+// fruits.push('Mango');
+// console.log(fruits);
+
+// fruits.pop();
+// console.log(fruits);
+
+// fruits.unshift('Mango');
+// console.log(fruits);
+
+// fruits.shift();
+// console.log(fruits);
+
+// let arr = [];
+
+// let num1 = Number (prompt('Enter the number'));
+// let num2 = Number (prompt('Enter the number'));
+// let num3 = Number (prompt('Enter the number'));
+
+// arr.push(num1, num2, num3);
+// console.log(arr);
+
+// let styles = ["Джаз", "Блюз"];
+// styles.push("Рок-н-рол");
+// styles[1] = "Классика";
+// let first = styles.shift();
+// console.log(first);
+// styles.unshift('Rep', 'Reggi');
+
+// console.log(styles);
+
+// for(let num = 0; num < 5; num++){
+//     console.log(num);
+//     if(num == 2){
+//         break;
+//     }
+// }
+
+// for(let num = 0; num < 5; num++){
+//     if(num == 2 || num == 4){
+//         continue;
+//     }
+//     console.log(num);
+// }
+
+// let arr = [1, 0, -3, 13, -45, 17, 99];
+
+// for(let num = 0; num < arr.length; num++){
+//     if(arr[num] > 0){
+//         console.log(arr[num]);
+//     }
+// }
+// let arr = [1, 0, -3, 13, -45, 17, 99];
+// let sum = 0;
+
+// for(let num = 0; num < arr.length; num++){
+//     if(arr[num] > 0){
+//         sum += arr[num];
+//     }
+// }
+// console.log(sum);
